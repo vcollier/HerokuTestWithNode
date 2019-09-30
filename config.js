1689e85742392a0e0213ca9076ce3fd20a3f6a35
Collier <v_collier@ats-hr420-dm23.fc.ca>
2019-09-30 19:36:28 -0400

    adding express and node stuff

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    uname: 'root',
+    upass: 'root', //windows leave blank
+    port: 8889, //windows 3306
+    host: 'localhost',
+    db: 'bizcard'
+
+}
+
+// make this a public file our app can read
+module.exports = config;
